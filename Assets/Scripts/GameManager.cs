834e858ef4c7e1ddbe8d0c2fdd9c4bb1a5ff568f
Rodrigo Ratochinski <rodrigo_ratochinski@hotmail.com>
2023-01-28 20:53:28 -0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool actionTurn;
+    void Start()
+    {
+        actionTurn = true;
+    }
+
+    void Update()
+    {
+        if(actionTurn == true)
+        {
+
+        }
+    }
+}
